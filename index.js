cf3cc00c5e092766cd0864cb4c5061fd5c875ce5
MD MOHABBAT ALI <mohabbatalit8@gmail.com>
2023-05-04 14:09:37 +0600

    data load

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
+
+const allChef = require('./data/fakeChefData.json');
+
+app.use(cors())
 
 app.get('/', (req, res) =>{
     res.send('data is comming')
+})
+
+app.get('/allChef', (req, res) =>{
+    res.send(allChef);
 })
 
 app.listen(port, () =>{
